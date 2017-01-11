8a4d7e0c559f6cf7a957ad25b1019c5fb2ba7667
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-01-10 19:20:23 -0800

    Constrained card width

--- src/Board.js
+++ src/Board.js
@@ -4,8 +4,10 @@ import ImageCard from './ImageCard';
 
 const dummyData = [];
 
+// TODO: Remove dummyData
+// eslint-disable-next-line
 for (let i = 0; i < 10; i++) {
-  dummyData.push(`http://placehold.it/200x${Math.floor(Math.random() * (500 - 200)) + 200}`);
+  dummyData.push(`http://placehold.it/${Math.floor(Math.random() * (500 - 200)) + 200}x${Math.floor(Math.random() * (500 - 200)) + 200}`);
 }
 
 class Board extends Component {
@@ -23,7 +25,7 @@ class Board extends Component {
             gutter: 10
           }}
         >
-          {dummyData.map(src => <ImageCard src={src} />)}
+          {dummyData.map((src, i) => <ImageCard key={i} src={src} />)}
         </Masonry>
       </div>
   );
