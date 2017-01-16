78697b2278f197cc5b24bee77ba9be64f254ec60
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-01-15 22:43:07 -0800

    Styled card

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -12,6 +12,21 @@ const ImageCard = ({ image }) => {
         <div className="content">
           {image.description}
         </div>
+        <div className="media">
+          <div className="media-left">
+            <figure>
+              <img src={image.userId.profilePhoto} alt={image.userId.userName} height="35" width="35" />
+            </figure>
+          </div>
+          <div className="media-content">
+            <button className="button is-primary is-outlined is-pulled-right">
+              <span className="icon is-small">
+                <i className="fa fa-star-o" />
+              </span>
+              <span>{image.likes.length}</span>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
