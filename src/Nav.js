0375e268741659d14556bf836d88c9769660fae3
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-01-18 18:19:21 -0800

    Close mobile menu after navigating

--- src/Nav.js
+++ src/Nav.js
@@ -9,10 +9,19 @@ class Nav extends Component {
       twitterLoading: false
     };
     this.toggleMenu = this.toggleMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
     this.twitterLoading = this.twitterLoading.bind(this);
+    this.logout = this.logout.bind(this);
   }
   toggleMenu() {
     this.setState({ mobileMenu: !this.state.mobileMenu });
+  }
+  closeMenu() {
+    this.setState({ mobileMenu: false });
+  }
+  logout() {
+    this.setState({ mobileMenu: false });
+    this.props.logout();
   }
   twitterLoading() {
     this.setState({ twitterLoading: true });
@@ -20,14 +29,14 @@ class Nav extends Component {
   // TODO: PROD - fix url
   // Duplicate checks to userId to prevent wrapping adjacent tags in jsx which screws up css
   render() {
-    const { logout, userId } = this.props;
+    const { userId } = this.props;
     const { mobileMenu, twitterLoading } = this.state;
     return (
       <nav className="nav has-shadow">
 
         <div className="container">
           <div className="nav-left">
-            <Link to="/" className="nav-item is-tab">
+            <Link to="/" className="nav-item is-tab" onClick={this.closeMenu}>
               Imageboard
             </Link>
           </div>
@@ -40,24 +49,24 @@ class Nav extends Component {
 
           {userId ?
             <div className={`nav-right nav-menu ${mobileMenu && 'is-active'}`}>
-              <Link to="/about" activeClassName="is-active" className="nav-item is-tab">
+              <Link to="/about" activeClassName="is-active" className="nav-item is-tab" onClick={this.closeMenu}>
                 About
               </Link>
-              <Link to="/add_image" activeClassName="is-active" className="nav-item is-tab">
+              <Link to="/add_image" activeClassName="is-active" className="nav-item is-tab" onClick={this.closeMenu}>
                     Add Image
               </Link>
-              <Link to={`/images/${this.props.userId}`} activeClassName="is-active" className="nav-item is-tab">
+              <Link to={`/images/${this.props.userId}`} activeClassName="is-active" className="nav-item is-tab" onClick={this.closeMenu}>
                     My Images
               </Link>
               <span className="nav-item">
-                <button type="button" className="button" onClick={logout} >
+                <button type="button" className="button" onClick={this.logout} >
                   <span>Logout</span>
                 </button>
               </span>
             </div>
           :
             <div className={`nav-right nav-menu ${mobileMenu && 'is-active'}`}>
-              <Link to="/about" activeClassName="is-active" className="nav-item is-tab">
+              <Link to="/about" activeClassName="is-active" className="nav-item is-tab" onClick={this.closeMenu}>
                 About
               </Link>
               <span className="nav-item">
